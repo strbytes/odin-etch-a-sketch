a0438c66fbefc1544f9ff627efc29e15b432fb5c
strbytes <strbytes@gmail.com>
2022-04-09 14:40:33 -0400

    Create grid on page load, format JS

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-let sketch = document.querySelector("#sketch")
-
+let sketch = document.querySelector("#sketch");
 function createDiv() {
-	let div = document.createElement("div")
-	div.setAttribute("class", "square")
-	return div
+  let div = document.createElement("div");
+  div.setAttribute("class", "square");
+  return div;
 }
 
 function createRow(len) {
-	let row = document.createElement("div")
-	row.setAttribute("class", "row")
-	for (let i = 0; i < len; i++) {
-		row.appendChild(createDiv())
-	}
-	return row
+  let row = document.createElement("div");
+  row.setAttribute("class", "row");
+  for (let i = 0; i < len; i++) {
+    row.appendChild(createDiv());
+  }
+  return row;
 }
 
 function createGrid(len) {
-	for (let i = 0; i < len; i++) {
-		sketch.appendChild(createRow(len))
-	}
+  for (let i = 0; i < len; i++) {
+    sketch.appendChild(createRow(len));
+  }
 }
+
+createGrid(16);
