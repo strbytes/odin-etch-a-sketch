a722167838b6e844121564a5fa1e2478efa21b1c
strbytes <strbytes@gmail.com>
2022-04-09 19:42:07 -0400

    Add color picker and random color features
    Some small tweaks to style and element ids.

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 let sketch = document.querySelector("#sketch");
-let buttons = document.querySelectorAll(".button")
-let clearButton = document.querySelector("#clear")
-let gridSizeSelector = document.querySelector("#grid-size")
-let gridSizeLabel = document.querySelector("label")
+let colorPicker = document.querySelector("#color-picker")
+let buttons = document.querySelectorAll(".button");
+let solidButton = document.querySelector("#solid-color");
+let randomButton = document.querySelector("#random-color");
+let clearButton = document.querySelector("#clear");
+let gridSizeSelector = document.querySelector("#grid-size");
+let gridSizeLabel = document.querySelector("#slider-label");
 
+let random = false;
+let color = colorPicker.value;
+
+colorPicker.addEventListener("change", (event) => {
+  color = event.target.value;
+  console.log(color);
+});
+solidButton.addEventListener("click", () => (random = false));
+randomButton.addEventListener("click", () => (random = true));
 clearButton.addEventListener("click", clearGrid);
 gridSizeSelector.addEventListener("change", gridSizeChange);
 gridSizeSelector.addEventListener("input", gridSizeLabelChange);
 
 for (let button of [...buttons]) {
-  if (button.id = "grid-slider") continue;
+  if (button.id === "grid-slider") continue;
   button.addEventListener("mousedown", (e) => {
-    e.target.classList.remove("button");
+    e.target.classList.remove("button")
     e.target.classList.add("button-click");
   });
   button.addEventListener("mouseup", (e) => {
@@ -46,7 +58,11 @@ function createGrid(len) {
 }
 
 function onMouseOver(event) {
-  event.target.style.backgroundColor = "black";
+  // color = "black";
+  if (random) {
+    color = "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+  }
+  event.target.style.backgroundColor = color;
 }
 
 function clearGrid() {
@@ -58,12 +74,12 @@ function clearGrid() {
 }
 
 function gridSizeChange(event) {
-  size = event.target.value
+  size = event.target.value;
   createGrid(size);
 }
 
 function gridSizeLabelChange(event) {
-  size = event.target.value
+  size = event.target.value;
   gridSizeLabel.textContent = `Grid size: ${size} x ${size}`;
 }
 
